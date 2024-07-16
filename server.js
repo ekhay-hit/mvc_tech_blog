ebcc568d35274cda86ece8aa5e04089915ed4265
Houssine <ekhay1@outlook.com>
2024-07-16 09:53:53 -0400

    require json parse and urlencode and require the path

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
 
-
+//initializing the port and app
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse json
+app.use(express.json());
+// getting params and queries from url
+app.use(express.urlencoded({extended:true}));
 
 
 
