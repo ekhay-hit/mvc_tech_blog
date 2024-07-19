66abeda5ce32bab559214d92b62512c122754bac
Houssine <ekhay1@outlook.com>
2024-07-18 21:24:09 -0400

    added User model main and login handlebars

--- server.js
+++ server.js
@@ -2,14 +2,31 @@ const express = require("express");
 const sequelize = require("./config/connection.js")
 const path = require("path");
 const model= require('./models/User.js')
+// handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 //initializing the port and app
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// setting the engine to use handlebars
+app.engine("handlebars",hbs.engine);
+// set the view to use handlebars
+app.set("view engine","handlebars")
+
+
 // parse json
 app.use(express.json());
 // getting params and queries from url
 app.use(express.urlencoded({extended:true}));
 
+// api
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/" ,async(req,res)=>{
+    res.render('login')
+})
 
 
 sequelize.sync().then(()=>{
