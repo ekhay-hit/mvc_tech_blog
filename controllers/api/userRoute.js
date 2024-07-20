e69cd3e03e48af07216e101a3b017f33580c21e6
Houssine <ekhay1@outlook.com>
2024-07-20 18:28:56 -0400

    implemented the post request for user login in the backend

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -16,7 +16,7 @@ router.get("/signup" ,async(req,res)=>{
 })
 
 // POST REQUEST FOR THE USER
-// post request for creating new user
+// post request for creating new user*******************************************************************
 
 router.post("/signup", async (req, res)=>{
     try{
@@ -36,7 +36,41 @@ router.post("/signup", async (req, res)=>{
         res.status(500).json(err);
 
     }
-} )
+});
+
+// post request for user logi ************************************************************************
+
+router.post("/login", async(req,res) =>{
+
+    try{
+        // find user email if there is data
+        const loginData = await User.findOne({where:{email:req.body.email}});;
+
+        if(!loginData){
+            res.status(404).json({message:"please provide your login credintial"})
+            return;
+        }
+     
+                // check if the password is valid 
+            const pwIsValid = await bcrypt.compare(
+                req.body.password, loginData.password
+            )    
+        
+        if (!pwIsValid){
+            res.status(400).json({message: "Wrong login credintial"})
+        }
+
+        res.status(200).json({message: "you are logged in successfully"})
+    
+        
+
+    }catch(err){
+        res.status(500).json({message:"500 server error"})
+        // res.status(500).json(err);
+
+    }
+
+})
 
 
 module.exports=router;
