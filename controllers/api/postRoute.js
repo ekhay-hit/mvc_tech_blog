4712a6b954fdec9deb44a059d89bce9223cb70a8
Houssine <ekhay1@outlook.com>
2024-08-06 16:07:36 -0400

    implemented the patch request to update the post

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -23,7 +23,42 @@ router.post("/", async (req, res) =>{
 })
 
 
-// ********************************* Get requist to return all the post 
+// ********************************* update the post 
+
+router.patch("/:id", async(req, res)=>{
+
+    try{
+        // retrieve the id from the url
+        const post_id= req.params.id;
+
+        // find the post using the id
+        const newPost = await Post.findByPk(post_id);
+
+        // if it is not exist return not found
+        if(!newPost){
+           return res.status(404).json({message:"no post found with requestd id"})   
+        }
+        
+        // continue to udpate the post
+        await Post.update({
+            title: req.body.title,
+            content:req.body.content,    
+        },
+        {
+            where:{
+                id:post_id,
+            }
+        })
+
+        // retrieve the post after it is updated and return the response
+        const updatedPost = await Post.findByPk(post_id);
+        res.status(200).json(updatedPost)
+
+    }catch(err){
+        res.status(500).json(err)
+       
+    }
+})
 module.exports = router;
 
 
